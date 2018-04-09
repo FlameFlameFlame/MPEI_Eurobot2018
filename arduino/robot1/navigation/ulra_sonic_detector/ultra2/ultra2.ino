a2cc557c91a6c9b0c430d1571c09d1ec5db0684f
ODN13 <qzircool@gmail.com>
2018-04-09 18:20:50 +0300

    сделали отправку данных на edison
    написали функцию считывания и передачи массива данных на edison
    сделали тестовый вывод на экран

---
+++ arduino/robot1/navigation/ulra_sonic_detector/ultra2/ultra2.ino
@@ -0,0 +1,47 @@
+#include <Ultrasonic.h>
+#include <Wire.h>
+#define  ULT1Trig 1 
+#define  ULT1Echo 2
+#define  ULT2Trig 3
+#define  ULT2Echo 4
+#define  ULT3Trig 5
+#define  ULT3Echo 6
+#define  ULT4Trig 7
+#define  ULT4Echo 8
+Ultrasonic ultrasonic1(ULT1Trig,ULT1Echo);
+Ultrasonic ultrasonic2(ULT2Trig,ULT2Echo);
+Ultrasonic ultrasonic3(ULT3Trig,ULT3Echo);
+Ultrasonic ultrasonic4(ULT4Trig,ULT4Echo);
+
+byte ultras[4];
+
+void Transfer ()
+  {
+    Wire.write(ultras, 4);            // отсылаемое значение  
+  }
+
+void setup() 
+{
+  Serial.begin (9600); // подключаем монитор порта
+  Wire.begin(5);
+  Wire.onRequest(Transfer); 
+}
+
+void loop() 
+{   
+  ultras[0] = map(ultrasonic1.distanceRead(),2,400,0,255);
+  ultras[1] = map(ultrasonic2.distanceRead(),2,400,0,255);
+  ultras[2] = map(ultrasonic3.distanceRead(),2,400,0,255);
+  ultras[3] = map(ultrasonic4.distanceRead(),2,400,0,255);
+  for (int i = 0; i < 4; i++)
+  {
+    Serial.print(ultras[i]);
+    Serial.print(' ');
+  
+  }
+  Serial.print(' ');
+  delay(1000);
+}
+
+
+
